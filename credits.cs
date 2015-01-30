7773112e1572750665ef9ec15edbe6e580f6673f
ATails <epicmannpeete2@gmail.com>
2015-01-30 06:59:49 -0800

    Update credits.cs

--- credits.cs
+++ credits.cs
@@ -1,2 +1,7 @@
 messageClient(%client, '', "\c6 - Credits -");
 messageClient(%client, '', "\c5 ATails - Coding");
+messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+messageClient(%client, '', "\c6 [Empty Vol. Spot]");
