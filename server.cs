babce22a95878495b7eddb011b9a9c0759a1a283
ATails <epicmannpeete2@gmail.com>
2015-01-30 06:53:24 -0800

    Update server.cs

--- server.cs
+++ server.cs
@@ -1,5 +1,9 @@
 package BLSonic
 {
+  function serverCmdcredits(%client)
+  {
+	 execute("credits.cs")
+}
 };
 activatePackage(BLSonic); // This will automatically parse the package on execution
 //Let the Sonic BEGIN!
