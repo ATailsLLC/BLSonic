be96d65ad177b934e3fbb6cbb889085553e70caa
ATails <epicmannpeete2@gmail.com>
2015-01-30 16:36:21 -0800

    Delete credits.cs

--- credits.cs
+++
@@ -1,7 +0,0 @@
-messageClient(%client, '', "\c6 - Credits -");
-messageClient(%client, '', "\c5 ATails - Coding");
-messageClient(%client, '', "\c6 [Empty Vol. Spot]");
-messageClient(%client, '', "\c6 [Empty Vol. Spot]");
-messageClient(%client, '', "\c6 [Empty Vol. Spot]");
-messageClient(%client, '', "\c6 [Empty Vol. Spot]");
-messageClient(%client, '', "\c6 [Empty Vol. Spot]");
