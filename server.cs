33d31f65249a85ec3d1edf40dd79fe9e8d874086
ATails <epicmannpeete2@gmail.com>
2015-01-30 16:37:28 -0800

    Update server.cs

--- server.cs
+++ server.cs
@@ -2,8 +2,15 @@ package BLSonic
 {
   function serverCmdcredits(%client)
   {
-	 exec("credits.cs")
-}
+     messageClient(%client, '', "\c6 - Credits -");
+     messageClient(%client, '', "\c5 ATails - Coding");
+     messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+     messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+     messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+     messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+     messageClient(%client, '', "\c6 [Empty Vol. Spot]");
+  }
 };
 activatePackage(BLSonic); // This will automatically parse the package on execution
 //Let the Sonic BEGIN!
+
